0c738964a981f77ca5a221fb10ae01bf4edf89dc
arun-singh-codes <arun.singh.codes@gmail.com>
2026-04-27 21:29:05 +0530

    oa3

--- frontend/src/landing_page/home/Hero.js
+++ frontend/src/landing_page/home/Hero.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import {Link} from "react-router-dom"
-
+import { useNavigate } from "react-router-dom";
 function Hero() {
+
+     const navigate = useNavigate();
     return ( 
         <div className="HeroContainer">
             <div className="Hero">
                 <img src="assets/images/homeHero.png"  alt="homehero"/>
                 <h4>Invest Like A Pro on MyStockly</h4>
                 <p>Invest smarter in stocks, mutual funds, and more — all in one place.</p>
-                <div className='button'><h2><Link to="/signup">Sign up for free</Link></h2></div>
+                <div className='button'><h2><button onClick={() => navigate("/signup")}>Sign up for free</button ></h2></div>
             </div>
         </div>
         
